0fb59a9db096f9c1c875f5890c4bd83c872562a9
Harish <harishkota0308@gmail.com>
2026-05-29 10:30:01 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+    # 👇 CHANGE 'project.settings' TO 'Birla.settings' HERE 👇
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Birla.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
